b97f0d830d45e915667beb0f2460dbab9a7165d4
showagon <43502134+showagon@users.noreply.github.com>
2020-01-27 02:15:14 -0800

    Update RegistratonFormApplication.java

--- src/main/java/com/abd/RegistratonForm/RegistratonFormApplication.java
+++ src/main/java/com/abd/RegistratonForm/RegistratonFormApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan("com.abd.RegistratonForm")
+@EntityScan("com.abd.model")
 public class RegistratonFormApplication {
 
 	public static void main(String[] args) {
